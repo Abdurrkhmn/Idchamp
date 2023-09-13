6569da55728ae6424cf62b1f1759f86ee4e96241
Abdurrkhmn <Abdurrahmansy25@gmail.com>
2023-09-13 11:46:55 +0300

    new

--- main.py
+++
@@ -1,9 +0,0 @@
-"""
-TODO:
-- Buatlah sebuah variabel bernama greeting.
-- Assign teks "Saya mulai belajar Python!" pada variabel tersebut. 
-"""
-"""
-greeting = 'Saya mulai belajar Python!'
-print(greeting)
-"""
